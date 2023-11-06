5dba18279fa4440eacfcf641243a25fc31e6a6da
Daniel <jumpypants10@gmail.com>
2023-11-05 18:25:31 -0800

    finished engine

--- scripts/user.js
+++ scripts/user.js
@@ -1,9 +1,9 @@
 var cam1 = new Camera(new V2(0, 0), new V2(500, 500), 0);
-var player = new Entity(new V2(0, 0), new V2(30, 50), 0, null);
+var player = new Entity(new V2(0, 0), new V2(30, 50), 0.5, null);
 
-scenes.set("scene1", new Scene([new Layer([player])]), [cam1]);
-screens.set("screen1", new Screen([new ScreenSection(new V2(300, 300), new V2(1, 1), 0, cam1, scenes.get("scene1"), defaultRenderer)], function(){}));
-systems.set("player system", new System([cam1], [new Component(function(component){}, function(system, dt){
+scenes.set("scene1", new Scene([new Layer([player, new Entity(new V2(0, 0), new V2(30, 50), -0.5, null)])]), [cam1]);
+screens.set("screen1", new Screen([new ScreenSection(new V2(300, 300), new V2(1, 1), 0, cam1, scenes.get("scene1"), defaultRenderer), new ScreenSection(new V2(700, 300), new V2(0.3, 0.3), 0.5, cam1, scenes.get("scene1"), defaultRenderer)], function(){}));
+systems.set("player system", new System([cam1, player], [new Component(function(system){}, function(system, dt){
     for(var part of system.parts){
         part.pos.x += 10 * dt;
     }
