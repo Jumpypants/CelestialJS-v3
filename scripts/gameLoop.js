59a81aa3a11011681946c06fa317d14f5474a6a2
Daniel <jumpypants10@gmail.com>
2023-11-05 17:06:42 -0800

    working i think

--- scripts/gameLoop.js
+++ scripts/gameLoop.js
@@ -1,6 +1,10 @@
 function gameLoop(){
     // Calculate the delta time.
     var dt = 1 / FPS;
+    // Call component tick functions.
     tickComponents(dt);
+    // Call the user's tick function.
     tick(dt);
+    // Draw the current screen.
+    screens.get(currentScreen).draw();
 }
---
+++ scripts/user.js
@@ -0,0 +1,3 @@
+scenes.set("scene1", new Scene([new Layer([])]), []);
+screens.set("screen1", new Screen([], function(){}));
+init();
