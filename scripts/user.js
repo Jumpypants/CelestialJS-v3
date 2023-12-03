62bb95d010a6e22fa417a496c6e4c8df97c4aad0
Daniel <jumpypants10@gmail.com>
2023-12-02 18:05:36 -0800

    fixed css body margin

--- scripts/user.js
+++ scripts/user.js
@@ -2,7 +2,7 @@ var cam1 = new Camera(new V2(0, 0), new V2(500, 500), 0, []);
 var player = new Entity(new V2(0, 0), new V2(120, 80), 0, document.getElementById("test_image"), []);
 
 scenes.set("scene1", new Scene([new Layer([player], 1)], [cam1]));
-screens.set("screen1", new Screen([new ScreenSection(new V2(300, 300), new V2(1, 1), 0, cam1, scenes.get("scene1"), defaultRenderer), new ScreenSection(new V2(700, 300), new V2(0.3, 0.3), 0.5, cam1, scenes.get("scene1"), defaultRenderer)], function(){}));
+screens.set("screen1", new Screen([new ScreenSection(new V2(250, 250), new V2(1, 1), 0, cam1, scenes.get("scene1"), defaultRenderer), new ScreenSection(new V2(700, 300), new V2(0.3, 0.3), 0.5, cam1, scenes.get("scene1"), defaultRenderer)], function(){}));
 systems.set("player system", new System([player], [new Component(function(system){}, function(system, dt){
     for(var part of system.parts){
         part.pos.x += 10 * dt;
